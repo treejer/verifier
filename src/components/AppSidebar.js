0341943dea009f7f144463b729dc0266ee9dd7a5
Behnam Jaberi <behnam_it87@yahoo.com>
2023-08-04 20:27:44 +0330

    Remove unused file, header component and router linksand assets

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
+import { CSidebar, CSidebarBrand, CSidebarNav } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { AppSidebarNav } from './AppSidebarNav'
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
+import { logo } from 'src/assets/brand/logo'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 import navigation from '../_nav'
@@ -12,20 +11,18 @@ import { useToggleSidebar } from '../redux/modules/init/slice'
 
 const AppSidebar = () => {
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const { sidebarShow, toggleSidebar } = useToggleSidebar()
+  const { sidebarShow } = useToggleSidebar()
 
   return (
     <CSidebar position="fixed" unfoldable={unfoldable} visible={sidebarShow}>
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
+        <CIcon className="sidebar-brand-full" icon={logo} height={100} />
       </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>
           <AppSidebarNav items={navigation} />
         </SimpleBar>
       </CSidebarNav>
-      <CSidebarToggler className="d-none d-lg-flex" onClick={toggleSidebar} />
     </CSidebar>
   )
 }

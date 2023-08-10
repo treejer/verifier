b6c1d09ea5d1be4c4a287907f8e4403cfe193060
Behnam Jaberi <behnam_it87@yahoo.com>
2023-08-10 16:30:43 +0330

    Update style variable and routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Users = React.lazy(() => import('./views/users/Users'))
+const UsersDetail = React.lazy(() => import('./views/users/id'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/users', name: 'Users', element: Users },
+  { path: '/users/:id', name: 'Users', element: UsersDetail },
 ]
 
 export default routes

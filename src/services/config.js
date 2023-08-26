14714dd1b97cb3f0dc34fabec38bdab54a50378d
Behnam Jaberi <behnam_it87@yahoo.com>
2023-08-26 18:13:16 +0330

    Update config and env file

--- src/services/config.js
+++ src/services/config.js
@@ -2,53 +2,59 @@ const blockChainConfig = {
   137: {
     contracts: {
       TreeFactory: {
-        address: 'a',
-        abi: '',
+        address: process.env.REACT_APP_POLYGON_CONTRACT_TREE_FACTORY_ADDRESS,
+        abi: require('../abis/TreeFactory.json'),
       },
-      Paymaster: {
-        address: '',
-        abi: '',
+      AR: {
+        address: process.env.REACT_APP_POLYGON_CONTRACT_AR_ADDRESS,
+        abi: require('../abis/AccessRestriction.json'),
       },
       Planter: {
-        address: '',
-        abi: '',
+        address: process.env.REACT_APP_POLYGON_CONTRACT_PLANTER_ADDRESS,
+        abi: require('../abis/Planter.json'),
       },
     },
+    exp_url: process.env.REACT_APP_POLYGON_EXPLORER_URL,
     base_url: process.env.REACT_APP_MAIN_MATIC_BASE_URL,
+    multiSign: process.env.REACT_APP_POLYGON_MULTISIGN,
   },
   5: {
     contracts: {
       TreeFactory: {
-        address: 'b',
-        abi: '',
+        address: process.env.REACT_APP_GOERLI_CONTRACT_TREE_FACTORY_ADDRESS,
+        abi: require('../abis/TreeFactory.json'),
       },
-      Paymaster: {
-        address: '',
-        abi: '',
+      AR: {
+        address: process.env.REACT_APP_GOERLI_CONTRACT_AR_ADDRESS,
+        abi: require('../abis/AccessRestriction.json'),
       },
       Planter: {
-        address: '',
-        abi: '',
+        address: process.env.REACT_APP_GOERLI_CONTRACT_PLANTER_ADDRESS,
+        abi: require('../abis/Planter.json'),
       },
     },
+    exp_url: process.env.REACT_APP_GOERLI_EXPLORER_URL,
     base_url: process.env.REACT_APP_MAIN_GOERLI_BASE_URL,
+    multiSign: process.env.REACT_APP_GOERLI_MULTISIGN,
   },
   80001: {
     contracts: {
       TreeFactory: {
-        address: 'b',
-        abi: '',
-      },
-      Paymaster: {
         address: '',
-        abi: '',
+        abi: require('../abis/TreeFactory.json'),
+      },
+      AR: {
+        address: '',
+        abi: require('../abis/AccessRestriction.json'),
       },
       Planter: {
         address: '',
-        abi: '',
+        abi: require('../abis/Planter.json'),
       },
     },
+    exp_url: process.env.REACT_APP_POLYGON_EXPLORER_URL,
     base_url: process.env.REACT_APP_TEST_MATIC_BASE_URL,
+    multiSign: false,
   },
 }
 
